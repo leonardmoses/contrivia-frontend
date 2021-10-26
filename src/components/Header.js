666bb03f16396ffcfa231805dd68df836a85814e
leonardmoses <leonard.moses@anemote.com>
2021-10-26 15:26:23 -0400

    header h1 takes user back to splash page.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 function Header(props) {
     return (
         <nav className="nav">
-            <h1>Contrivia</h1>
+            <Link to="/"><h1>Contrivia</h1></Link>
             <ul>
                 <li><Link to="/category"><button>Category</button></Link></li>
                 <li><Link to="/new"><button>New</button></Link></li>
